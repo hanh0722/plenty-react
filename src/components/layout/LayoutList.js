6b95544721d2d959101858134d149e373c954162
Brian07 <hoanganh0722@hanh0722.local>
2021-08-28 21:49:33 +0700

    upload day2

--- src/components/layout/LayoutList.js
+++ src/components/layout/LayoutList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './LayoutList.module.scss';
-
 const LayoutList = props =>{
     return (
         <div className={`${props.className} ${styles.layout} ${props.isClicked && styles.back}`}>
+            <p onClick={props.setBack} className={styles.icon}><i className="fal fa-arrow-left"></i> Back</p>
             {props.children}
         </div>
     )
